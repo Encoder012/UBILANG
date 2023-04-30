20c7e141c3e869519af81acde543bde9621ebe3b
Naman <namanb454@gmail.com>
2023-04-30 20:06:30 +0530

    1st commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,9 +7,9 @@ function Profile() {
       <div className="lg:w-4/6 mx-auto" id=''>
         <div className="flex flex-col sm:flex-row mt-10  " id='profilecard'>
           <div className="sm:w-1/3 text-center sm: sm:py-8">
-            <div>
-              <div className="w-28 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                <img className='rounded-full' src='chloeCarter.png' />
+            {/* <div> */}
+              <div className="w-28 h-20 mt-10 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                <img className='rounded-full' src='chloeCarter.png' alt='user'/>
 
               </div>
 
@@ -17,11 +17,11 @@ function Profile() {
                 <button className='hover:text-black'>
                   <MdCameraEnhance className='hover:text-black' id='profileChange' />
                 </button>
-                <h2 className="font-medium title-font mt-4 text-gray-800 text-base">Chloe Carter</h2>
+                <h2 className="font-medium title-font mb-3 text-gray-800 text-base">Chloe Carter</h2>
                 <hr />
               </div>
 
-            </div>
+            {/* </div> */}
 
             <h2 className='font-semibold text-2xl mt-5 text-gray-900'>Subscription</h2>
             <div className="rounded-lg h-44 w-60 m-auto mt-5 overflow-hidden bg-white">
@@ -46,17 +46,17 @@ function Profile() {
 
           <div className="sm:w-2/3 sm:pl-8 sm:py-2 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
             <h2 className='font-bold text-2xl text-gray-900'>My Account Settings</h2>
-            <h2 className='font-base text-2xl mt-3 text-gray-900'>Profile</h2>
+            <h2 className='font-base text-2xl mt-3 font-semibold text-gray-900'>Profile</h2>
             <section className="text-gray-600 body-font relative">
               <div className="container">
                 <div className="flex flex-col w-full px-3 mb-5 mt-5">
 
                   <p className="lg:w-2/3 leading-relaxed text-sm">Change Your Name</p>
                 </div>
-                <div className=" mx-">
+                <div className="">
                   <div className="flex flex-wrap -m-2">
                     <div className="px-5 w-1/2">
-                      <div className="relative px-">
+                      <div className="relative">
                         <label for="name" className="leading-7 text-base font-semibold text-gray-900">First Name</label>
                         <input type="text" id="firstname" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                           placeholder='User First Name' />
@@ -78,19 +78,23 @@ function Profile() {
 
 
                     <div className="flex flex-col w-full px-5 -mb-2 mt-5">
-                      <p className="lg:w-2/3 leading-relaxed text-sm">Change Your Email</p>
-                    </div>
-                    <div className="px-5 pt-5 w-1/2">
-                      <div className="relative">
-                        <label for="email" className="leading-7 text-base font-semibold text-gray-900">Email Address</label>
+                      <p className="lg:w-2/3 leading-relaxed text-base text-black font-semibold">Change Your Email</p>
+                    
+
+                    <div className="flex w-full px- mt- ">
+                      <div className="relative w-1/2 pr-5 ">
+                        {/* <label for="email" className="leading-7 text-base font-semibold text-gray-900">Email Address</label> */}
                         <input type="email" id="email" name="email"
                           className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                           placeholder='User Email'
                         />
+                      {/* </div> */}
                       </div>
-                    </div>
-                    <div className="px-5 mt-2  w-full">
+                    {/* </div> */}
+                    <div className="px-5 mt-auto w-fit">
                       <button className="flex text-black py-2 px-8 focus:outline-none hover:bg-indigo-100 rounded-full border-2 border-indigo-500 hover:border-x-8 hover:border-x-indigo-900 hover:border-y-indigo-900  text-sm">Save</button>
+                    </div>
+                    </div>
                     </div>
 
                     <div className="flex flex-col w-full px-5 mb-2 mt-5">
